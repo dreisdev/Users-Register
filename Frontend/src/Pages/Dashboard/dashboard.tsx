1b86f664f2ca76c52e52dffe9d41aea3a8c71fb9
dreisdev <dreis.dev@outlook.com>
2024-02-01 17:13:48 -0300

    personDelete frontend.

--- Frontend/src/Pages/Dashboard/dashboard.tsx
+++ Frontend/src/Pages/Dashboard/dashboard.tsx
@@ -9,10 +9,26 @@ import api from "../../Api/fetchData";
 import { useEffect, useState } from "react";
 import { format } from "date-fns";
 import { Person } from "../../@types/personTypes";
+import UseToast from "../../Hooks/useToast";
+import Modal from "../../Components/Modal/modal";
+
+// import { useParams } from "react-router-dom";
 
 const Dashboard = () => {
   const [dataPerson, setDataPerson] = useState<Person | undefined>();
+  const [isModalOpen, setModalOpen] = useState<boolean>(false);
+
+  const openModal = async () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    fetchPersonData();
+  };
   // const navigate = useNavigate();
+
+  // const { id } = useParams();
 
   const token = GetToken();
   const userId = GetUserId();
@@ -43,23 +59,52 @@ const Dashboard = () => {
 
   const HandleEdit = () => {
     console.log("handleEdit");
+
+    openModal();
   };
 
-  const HandleDelete = () => {
-    console.log("handleDelete");
+  const HandleDelete = async (personId: string) => {
+    console.log("id extraido:", personId);
+
+    try {
+      const response = await api.delete(`/excluirPF/${userId}/${personId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      UseToast(`${response.data.mensagem}`);
+
+      fetchPersonData();
+
+      console.log(response.data);
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      console.log(error);
+
+      UseToast(`Erro: ${error.response.data.mensagem}`, "error");
+    }
   };
+
   return (
     <>
       <div className="container-main">
         <main>
-          <button className="btn-green" id="btn-new-contact">
+          <button
+            className="btn-green"
+            id="btn-new-contact"
+            onClick={() => openModal()}
+          >
             Adicionar
           </button>
+
+          <Modal isOpen={isModalOpen} onClose={closeModal} />
 
           <div className="table">
             <div className="table-header">
               <div className="header-column">
-                <strong>Nome</strong>
+                <strong>Nome </strong>
               </div>
               <div className="header-column">
                 <strong>Sobrenome</strong>
@@ -101,7 +146,11 @@ const Dashboard = () => {
 
                       <div className="line-column action">
                         <img src={Edit} alt="edit" onClick={HandleEdit} />
-                        <img src={Delete} alt="delete" onClick={HandleDelete} />
+                        <img
+                          src={Delete}
+                          alt="delete"
+                          onClick={() => HandleDelete(person._id as string)}
+                        />
                       </div>
                     </div>
                   ))
